e57742406569db863cb6f1cf9a7bffdfc03f8aea
Byron Weiss <byron.weiss@gmail.com>
2017-02-15 18:04:12 +0000

    assignment 02 done

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,34 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[0]
   end
 
   def enqueue(element)
+    # Add element to the tail
+    i = @queue.length
+    @queue[i] = element
+    @head = @queue[0]
+    @tail = @queue[i]
   end
 
   def dequeue
+  # SET temp to the first element
+  empty? ? @temp = nil : @temp = @queue[0]
+  # Left-Shift the list by one entry
+  if empty? || @queue.length == 1
+    @queue = []
+  else
+    @queue = @queue[1..@queue.length]
+  end
+  p @queue
+  # RETURN temp
+  @head = @queue[0]
+  @tail =  @queue[@queue.length]
+  return @temp
   end
 
   def empty?
+    @queue.length <= 0 ? true : false
   end
 end
