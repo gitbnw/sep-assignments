6e567aac590021232beb3c5f145c4af76f24736c
Byron Weiss <byron.weiss@gmail.com>
2017-03-30 17:14:54 +0000

    I was supposed to do stacks too?

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,29 @@ class MyStack
   end
 
   def push(item)
+    @stack[@stack.length] = item
+    self.top = item
   end
 
   def pop
+    unless @stack == nil
+      @temp = self.top
+      if @stack.length - 2 > -1
+        @stack =  @stack[0..@stack.length - 2]
+        self.top = @stack[@stack.length - 1]
+      else
+        @stack = nil
+        self.top = nil
+      end
+      return @temp
+    end
   end
 
   def empty?
+    if @stack.length == 0
+      true
+    else
+      false
+    end
   end
 end
